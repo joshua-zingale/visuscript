4b2161a98e6b409ded1a4051b146c1eb3578dd41
Joshua Zingale <jtzingale@gmail.com>
2025-04-24 23:55:12 -0700

    refactoring

---
+++ src/structure.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Structure; 
+// {
+//     spawnpoint: Vec3,
+// }
+
+// impl Structure {
+//     fn new(spawnpoint: Vec3) -> (Self, Transform, Visibility) {
+//         (Structure {spawnpoint}, Transform::default(), Visibility::default())
+//     }
+// }
