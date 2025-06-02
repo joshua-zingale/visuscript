c43f9a46f820ad517808275c5cdaa02badd304f5
Joshua Zingale <jtzingale@gmail.com>
2025-06-02 13:27:37 -0700

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+import os
+def get_dependencies():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    requirements_path = os.path.join(dir_path, 'requirements.txt')
+    with open(requirements_path, "r") as f:
+        dependencies = list(filter(lambda x: len(x) > 0 and not x.startswith("#"), map(lambda x: x.strip(), f.readlines())))
+    return dependencies
 
 setup(
     name='visuscript',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=get_dependencies(),
     entry_points={
         'console_scripts': [
             'visuscript = visuscript.cli.visuscript_cli:main',
