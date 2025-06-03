bba2e5a1485163c08dd802983805aa085b4014bb
Joshua Zingale <jtzingale@gmail.com>
2025-06-03 13:41:02 -0700

    Improved type hin for unit_diff

--- visuscript/math_utility.py
+++ visuscript/math_utility.py
@@ -6,16 +6,15 @@ import numpy as np
 _VecType = TypeVar('_VecType', bound='Vec')
 
 
-def magnitude(vec: Vec):
+def magnitude(vec: Vec) -> float:
     return vec.dot(vec)**0.5
 
 def unit_diff(vec1: _VecType, vec2: _VecType) -> _VecType:
     diff = vec1 - vec2
     norm = magnitude(diff)
     if norm == 0:
-        return Vec(*(0 for _ in vec1))
+        return type(vec1)(*(0 for _ in vec1))
     return diff/magnitude(diff)
-
 
 def invert(matrix: Sequence[Sequence[float]]) -> Sequence[Sequence[float]]:
     return np.linalg.inv(matrix).tolist()
