a700192db71498cafb12fb8508eeb404177aa446
Joshua Zingale <jtzingale@gmail.com>
2025-06-12 08:14:59 -0700

    Added code display to lazy_animation and added lazy argument example

--- examples/lazy_animation.py
+++ examples/lazy_animation.py
@@ -1,50 +1,65 @@
 """
-LazyAnimation allows the initialization of an Animation to be delayed until its first advance.
+.lazy and :class:`LazyAnimation` allow the initialization of an Animation to be delayed until its first advance.
 This helps in cases where two animations are sequenced in a way that the initialization arguments
 for the second animation depend on the final state resultant from the first animation.
 
-This example shows the difference between sequencing animations with and without LazyAnimation.
+This example shows the difference between sequencing animations with and without .lazy and LazyAnimation.
 The goal is to move the circle first down and then to the right.
 """
 from visuscript import *
 from visuscript.animation import LazyAnimation
-
+from visuscript.code import get_all_code_blocks, PythonText
+code_blocks = get_all_code_blocks(__file__)
 scene = Scene()
 text = Text("Without LazyAnimation: Incorrect Sequencing").set_anchor(Anchor.TOP_LEFT).translate(*scene.xy(0.02,0.02))
 scene << text
+##1
 with scene as s:
     circle = Circle(20)
     s << circle
-    # Without LazyAnimation, the second translation will begin where the first did
-    # leading to a jump in the animation.
     s.animations << AnimationSequence(
         TranslationAnimation(circle.transform, circle.transform.translation + [0, 75, 0]),
         TranslationAnimation(circle.transform, circle.transform.translation + [100, 0, 0]),
         NoAnimation()
     )
+    s << PythonText(code_blocks[1], font_size=6).set_anchor(Anchor.TOP_LEFT).translate(*s.shape.top_left + [10,22.5])
+##
 
 text.set_text("With .lazy: Half Correct Sequencing")
-
+##2
 with scene as s:
     circle = Circle(20)
     s << circle
-    # With .lazy, the second translation's starting point is where the previous left off.
-    # This is correct, but the destination is still relative to the pre-animated circle.
     s.animations << AnimationSequence(
         TranslationAnimation(circle.transform, circle.transform.translation + [0, 75, 0]),
         TranslationAnimation.lazy(circle.transform, circle.transform.translation + [100, 0, 0]),
         NoAnimation()
     )
+    s << PythonText(code_blocks[2], font_size=6).set_anchor(Anchor.TOP_LEFT).translate(*s.shape.top_left + [10,22.5])
+##
 
-text.set_text("With LazyAnimation: Correct Sequencing")
+text.set_text("With .lazy & lazy argument: Correct Sequencing")
+##3
 with scene as s:
     circle = Circle(20)
     s << circle
-    # We can also use LazyAnimation, which will behave differently than .lazy
-    # because all arguments, including the destination, are evaluated lazily.
-    # This will lead to the intended sequence: down and then to the right.
+    s.animations << AnimationSequence(
+        TranslationAnimation(circle.transform, circle.transform.translation + [0, 75, 0]),
+        TranslationAnimation.lazy(circle.transform, circle.transform.lazy.translation.add([100, 0, 0])),
+        NoAnimation()
+    )
+    s << PythonText(code_blocks[3], font_size=6).set_anchor(Anchor.TOP_LEFT).translate(*s.shape.top_left + [10,22.5])
+##
+
+text.set_text("With LazyAnimation: Correct Sequencing")
+##4
+with scene as s:
+    circle = Circle(20)
+    s << circle
     s.animations << AnimationSequence(
         TranslationAnimation(circle.transform, circle.transform.translation + [0, 75, 0]),
         LazyAnimation(lambda:TranslationAnimation(circle.transform, circle.transform.translation + [100, 0, 0])),
         NoAnimation()
     )
+    s << PythonText(code_blocks[4], font_size=6).set_anchor(Anchor.TOP_LEFT).translate(*s.shape.top_left + [10,22.5])
+##
