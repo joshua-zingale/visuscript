424701dc53233601b136488b45247db44b8eebf1
Joshua Zingale <jtzingale@gmail.com>
2025-06-28 19:06:28 -0700

    Moving stuff around

---
+++ visuscript/drawable/__init__.py
@@ -0,0 +1,18 @@
+"""Contains things that can be drawn"""
+
+from .element import (
+    Circle,
+    Rect,
+    Image,
+)
+from .text import Text
+from .code import PythonText
+
+from .canvas import Scene
+
+from .connector import (
+    Anchor,
+    Line,
+    Arrow,
+    Edges,
+)
