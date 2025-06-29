befdb1ecb9a851fe27693802798723ffbf215edb
Joshua Zingale <jtzingale@gmail.com>
2025-06-28 19:11:32 -0700

    Removed unused code

--- visuscript/config.py
+++ visuscript/config.py
@@ -71,33 +71,3 @@ As an parameter type hint, specifies that passing in `DEFER_TO_CONFIG` as the ar
 
 DEFER_TO_CONFIG: ConfigurationDeference = object()
 """Indicates that this parameter should be set by the global configuration."""
-
-
-
-
-# class _Config:
-#     def __init__(self):
-#         self._data = {}
-
-#     def __getattr__(self, name):
-#         if name not in self._data:
-#             self._data[name] = _Config()
-#         return self._data[name]
-
-#     def __setattr__(self, name, value):
-#         if name == '_data':
-#             super().__setattr__(name, value)
-#         else:
-#             self._data[name] = value
-
-#     def __delattr__(self, name):
-#         if name in self._data:
-#             del self._data[name]
-
-#     def __repr__(self):
-#         return f"<Config {self._data}>"
-
-# config: _Config
-# """
-# The singleton configuration object for Visuscript, which sets defaults for various Visuscript features.
-# """
