5ed4d90474ed8f6b167f2d5abf831fcb17153a4e
Joshua Zingale <jtzingale@gmail.com>
2025-07-29 18:05:38 -0700

    Refactoring

--- examples/connector.py
+++ examples/connector.py
@@ -3,8 +3,10 @@ from visuscript.drawable.connector import Arrow, LineTarget
 
 s = Scene()
 c1 = Circle(10).translate(y=50)
-c2 = Circle(10).translate(100,50)
+c2 = Circle(10).translate(100, 50)
 l = Arrow(source=c1, destination=c2, source_target=LineTarget.CENTER)
 s << (c1, c2, l)
 
-s.player << PathAnimation(c2.transform, Path().M(*c2.global_shape.center).Q(0,-100,-100,0))
+s.player << PathAnimation(
+    c2.transform, Path().M(*c2.global_shape.center).Q(0, -100, -100, 0)
+)
